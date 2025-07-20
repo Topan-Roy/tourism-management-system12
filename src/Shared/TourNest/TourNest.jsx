dd6efabb13210bcecc8d3d06189517857810bed9
Topan-Roy <roytopan734@gmail.com>
2025-07-21 01:46:07 +0600

    added TourNest

--- src/Shared/TourNest/TourNest.jsx
+++ src/Shared/TourNest/TourNest.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import logo from '/images.png'; 
+import { Link } from 'react-router'; 
 
 const TourNest = () => {
   return (
     <div>
-      
+      <Link to="/">
+        <div className="flex items-end gap-1">
+          <img className="mb-1 h-10 w-10 object-cover rounded-full" src={logo} alt="TourNest Logo" />
+          <p className="text-3xl font-extrabold text-gray-800 dark:text-white">TourNest</p>
+        </div>
+      </Link>
     </div>
   );
 };
